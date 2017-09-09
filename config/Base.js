aa19ba07932bc782fbbd69a34344ee274116ff75
jiewj <jie_wj@126.com>
2017-09-09 10:24:58 +0800

    dev

--- config/Base.js
+++ config/Base.js
@@ -91,7 +91,7 @@ class WebpackBaseConfig {
 
       output: {
         path: path.resolve('./dist'),
-        filename: 'js/[name].js',
+        filename: 'js/[name].[hash].js',
         //[name].[chunkhash].js
         // publicPath: 'http://club.com/'
       },
